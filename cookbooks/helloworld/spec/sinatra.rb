170a2a353a59a89e88f2291f7faeab78e864268c
sumeshkanayi <sumeshkanayi@gmail.com>
2015-12-14 17:19:39 +0530

    Create sinatra.rb

---
+++ cookbooks/helloworld/spec/sinatra.rb
@@ -0,0 +1,14 @@
+require 'serverspec'
+
+# Required by serverspec
+set :backend, :exec
+
+describe "sinatra" do
+
+
+describe file('/var/lib/gems/1.9.1/gems/sinatra-1.4.6/lib/sinatra/base.rb') do
+  it { should exist }
+end
+
+
+end
