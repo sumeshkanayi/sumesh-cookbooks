1f4882429c9e0b94a24e41a7a7e84a525b3a475f
sumeshkanayi <sumeshkanayi@gmail.com>
2015-12-14 17:35:40 +0530

    Update sinatra.rb

--- cookbooks/helloworld/spec/sinatra.rb
+++ cookbooks/helloworld/spec/sinatra.rb
@@ -6,7 +6,7 @@ set :backend, :exec
 describe "sinatra" do
 
 
-describe file('/var/lib/gems/1.9.1/gems/sinatra-1.4.6/lib/sinatra/basesu.rb') do
+describe file('/var/lib/gems/1.9.1/gems/sinatra-1.4.6/lib/sinatra/bases.rb') do
   it { should exist }
 end
 
